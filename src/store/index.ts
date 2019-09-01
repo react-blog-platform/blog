d1d3f9f5e63b3503445fd06ff0fb3b3eef3b008c
lizhixiang <lizhixiang@egova.com.cn>
2019-09-01 23:49:10 +0800

    [M]引入redux-observable

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,16 +4,20 @@ import axiosMiddleware from 'redux-axios-middleware';
 import thunk from 'redux-thunk'
 import reducers from '../reducers'
 import {middlewareConfig} from '../config/interceptor'
+import { createEpicMiddleware } from 'redux-observable';
 
 const client = axios.create({ //all axios can be used, shown in axios documentation
   baseURL:'http://localhost:8080/api',
   responseType: 'json'
 });
 
+const epicMiddleware = createEpicMiddleware();
+
 const store = createStore(
   reducers, //custom reducers
   applyMiddleware(
     thunk,
+    epicMiddleware,
     axiosMiddleware(client,middlewareConfig)
   )
 )
